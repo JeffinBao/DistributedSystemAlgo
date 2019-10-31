cebe5125fa5cb3dd27e85e6390fbfe73d59c9af4
baojianfeng <bjf13609846991@gmail.com>
2019-10-30 20:58:48 -0400

    output log into separate files

--- here_we_go.sh
+++ here_we_go.sh
@@ -2,11 +2,11 @@
 
 #file_root_path=$(pwd)
 # find all java source files
-find . -name "*.java" > sources_java.txt
+find ./src/main -name "*.java" > sources_java.txt
 # print all java source file names
 cat sources_java.txt
 # compile all java source files
-javac @sources_java.txt
+javac -cp ./lib/*:. @sources_java.txt
 
 osascript <<END
 tell application "iTerm2"
@@ -19,19 +19,19 @@ tell application "iTerm2"
       # delay is important, it lets the window initialize and be able to execute following command
       delay 0.5
       write text "cd ./src/main/java"
-      write text "java MainApp server " & i
+      write text "java -Dlog4j.configurationFile=../resources/log4j2.xml -cp ../../../lib/*:. MainApp server " & i
     end repeat
     # move down
     tell application "System Events" to key code 125 using {command down, option down}
     # create clients
     delay 0.5
     write text "cd ./src/main/java"
-    write text "java MainApp client 0"
+    write text "java -Dlog4j.configurationFile=../resources/log4j2.xml -cp ../../../lib/*:. MainApp client 0"
     repeat with i from 0 to 3
       tell application "System Events" to keystroke "d" using {command down}
       delay 0.5
       write text "cd ./src/main/java"
-      write text "java MainApp client " & (i+1)
+      write text "java -Dlog4j.configurationFile=../resources/log4j2.xml -cp ../../../lib/*:. MainApp client " & (i+1)
     end repeat
     # init all connections by executing "connect" and moving left
     repeat with i from 0 to 3
@@ -48,20 +48,13 @@ tell application "iTerm2"
       write text "start"
       tell application "System Events" to keystroke "]" using {command down}
     end repeat
-    delay 0.5
+    delay 0.2
     write text "start"
 
     # close all connections and shut down servers， 80 seconds are estimated program running time
     # if each client executes 25000 operations. This is not a good solution, but for debugging purpose,
     # it can work.
-    delay 80
-    repeat with i from 0 to 6
-      write text "close"
-      tell application "System Events" to keystroke "[" using {command down}
-      delay 0.5
-    end repeat
-    # close the last server
-    write text "close"
+
   end tell
 end tell
 END
